64bf8a9e56205cf013faab976fa7101b45cd8d56
Embie Alieva <emutlueali@educacion.navarra.es>
2025-07-16 11:37:18 +0200

    Primer Hola Mundo

---
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -0,0 +1,51 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("======== Hello, Word! Sin salto de línea ========");
+
+        // Este es un comentario en C#
+
+        string str1 = "             >  .- \" -.   .-\\               ";
+        string str2 = "             ``--._`-...-'\"                ";
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine(".-.                                        ");
+        Console.WriteLine(@"|  \                                    __ ");
+        Console.WriteLine(@"(   `\                                 /  |");
+        Console.WriteLine(@" \    \                               /   |");
+        Console.WriteLine(@"  |  | `\                            /    /");
+        Console.WriteLine(@"  |  |\  \                         /` /  / ");
+        Console.WriteLine(@"  |  | \  \                     ./ '  /  / ");
+        Console.WriteLine(@"  |  |  \  \                   / _ /|  |   ");
+        Console.WriteLine(@"  |  |   \  \                / '  _/  |  | ");
+        Console.WriteLine(@"  |  |    \  \             / '   /   /   ) ");
+        Console.WriteLine(@"  | (      \  \           /    /   /   /   ");
+        Console.WriteLine(@"  (   \     \  \         /    /   /   /    ");
+        Console.WriteLine(@"   \   \     )  )       /    /   /   /     ");
+        Console.WriteLine(@"    \   \    |  |      /    /   /   /      ");
+        Console.WriteLine(@"     \   \   |  |     /    /   /   /       ");
+        Console.WriteLine(@"      \   \  |  |    /    /   /   /        ");
+        Console.WriteLine(@"       \   \ |  | (    /   /  / '          ");
+        Console.WriteLine(@"        \   \|  |   |   /  / ' /'          ");
+        Console.WriteLine(@"         \   \  /`. |`. \/ ' /'            ");
+        Console.WriteLine(@"          \     `-; -; -;   / '            ");
+        Console.WriteLine(@"           `\            <                 ");
+        Console.WriteLine(str1);
+        Console.WriteLine(@"            / .' ,__   `   \               ");
+        Console.WriteLine(@"           |     |  \      /\              ");
+        Console.WriteLine(@"           |     | __ \    | _ \           ");
+        Console.WriteLine(@"      _.------.._ | o\ |   | o`|\_         ");
+        Console.WriteLine("   ,-`.          `. | |   |  | ''`'-._._   ");
+        Console.WriteLine("  _.- '.            `:_|.__|/`'.-- - ; _ ` ");
+        Console.WriteLine(@" '   '     ``--.```--.. \/`..--'''  ; `-.  ");
+        Console.WriteLine("  .-``.      -.,-..__._.._._.__.   ;`-.    ");
+        Console.WriteLine(" '    `.        `;   |  | |      .'    `   ");
+        Console.WriteLine("       `.         `-.|  | | _'             ");
+        Console.WriteLine("         `.._.  `--''`_.- '                ");
+        Console.WriteLine(str2);
+        Console.WriteLine("        jgs      ;:    ;");
+    }
+}
