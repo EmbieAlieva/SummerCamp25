463a8971bbcb092ef8bb9773bf0b242b44223733
EmbieAlieva <emutlueali@educacion.navarra.es>
2025-07-17 10:29:37 +0200

    He creado el ejercicio de sacar los días de la semana si se introducen los números del 1 al 7 y si es 0, se sale del programa.

--- HolaMundoSolucion/HolaMundoProyecto/Program.cs
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Numerics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -47,5 +49,62 @@
         Console.WriteLine("         `.._.  `--''`_.- '                ");
         Console.WriteLine(str2);
         Console.WriteLine("        jgs      ;:    ;");
+
+
+        string numStr = "";
+        int num = -1;
+        bool esValorValido = true;
+
+        while (numStr != "0" || (num > 7 && num <= 1) || esValorValido == false)
+        {
+            Console.WriteLine("\nIntroduce un número del 1 al 7 (0 para salir): ");
+
+            numStr = Console.ReadLine();
+
+            string msj = "";
+
+            if ((String.IsNullOrEmpty(numStr.Trim()) || !int.TryParse(numStr, out num)))
+            {
+                msj = "Solo puede introducir números del 0 al 7";
+                esValorValido = false;
+            }
+            else if (num == 0)
+            {
+                msj = "¡Hasta luego!";
+                break;
+            }
+            else
+            {
+                esValorValido = true;
+                switch (num)
+                {
+                    case 1:
+                        msj = "Lunes";
+                        break;
+                    case 2:
+                        msj = "Martes";
+                        break;
+                    case 3:
+                        msj = "Miércoles";
+                        break;
+                    case 4:
+                        msj = "Jueves";
+                        break;
+                    case 5:
+                        msj = "Viernes";
+                        break;
+                    case 6:
+                        msj = "Sábado";
+                        break;
+                    case 7:
+                        msj = "Domingo";
+                        break;
+                    default:
+                        msj = "Valor no válido, intenta de nuevo.";
+                        break;
+                }
+            }              
+            Console.WriteLine(msj);
+        }
     }
 }
